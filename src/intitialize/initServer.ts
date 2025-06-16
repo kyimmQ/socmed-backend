6cd890589d3c3442bdf69ebeeba582b9f2464f59
Cac Le <cacphuong.le@gmail.com>
2025-06-16 17:16:08 +0000

    Structure setup

---
+++ src/intitialize/initServer.ts
@@ -0,0 +1,31 @@
+import http from "http";
+import { Application } from "express";
+
+export class Server {
+  private app: Application;
+  constructor(app: Application) {
+    this.app = app;
+  }
+
+  public start(): void {
+    this.securityMiddleware(this.app);
+    this.standarMiddleware(this.app);
+    this.routeMiddleware(this.app);
+    this.globalErrorHandler(this.app);
+    this.startServer(this.app);
+  }
+
+  private securityMiddleware(app: Application): void {}
+
+  private standarMiddleware(app: Application): void {}
+
+  private routeMiddleware(app: Application): void {}
+
+  private globalErrorHandler(app: Application): void {}
+
+  private startServer(app: Application): void {}
+
+  private createSocketIO(httpServer: http.Server): void {}
+
+  private startHttpServer(httpServer: http.Server): void {}
+}
