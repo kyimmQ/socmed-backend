ba2e1d3a7c336a6cda6a30993c765351240daf78
Cac Le <cacphuong.le@gmail.com>
2025-06-16 17:50:01 +0000

    Adding security middleware

--- src/intitialize/initServer.ts
+++ src/intitialize/initServer.ts
@@ -1,5 +1,11 @@
 import http from "http";
-import { Application } from "express";
+import { Application, json, urlencoded } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import hpp from "hpp";
+import compression from "compression";
+import cookieSession from "cookie-session";
+import HTTP_STATUS from "http-status-codes";
 
 export class Server {
   private app: Application;
@@ -9,15 +15,37 @@ export class Server {
 
   public start(): void {
     this.securityMiddleware(this.app);
-    this.standarMiddleware(this.app);
+    this.standardMiddleware(this.app);
     this.routeMiddleware(this.app);
     this.globalErrorHandler(this.app);
     this.startServer(this.app);
   }
 
-  private securityMiddleware(app: Application): void {}
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: "sesson",
+        keys: [],
+        maxAge: 24 * 60 * 60 * 1000, // 24 hours
+        secure: false,
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: "*",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      })
+    );
+  }
 
-  private standarMiddleware(app: Application): void {}
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: "10mb" }));
+    app.use(urlencoded({ limit: "10mb", extended: true }));
+  }
 
   private routeMiddleware(app: Application): void {}
 
